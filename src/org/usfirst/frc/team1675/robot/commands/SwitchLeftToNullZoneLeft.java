a6cc1a41573467a966a2c979597e2589b406a4f0
GREEN KNIGHT <greenknight6969@team1675.com>
2018-02-10 16:08:39 -0600

    added more values to path commmands C-A and D-B

--- src/org/usfirst/frc/team1675/robot/commands/SwitchLeftToNullZoneLeft.java
+++ src/org/usfirst/frc/team1675/robot/commands/SwitchLeftToNullZoneLeft.java
@@ -7,24 +7,28 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class SwitchLeftToNullZoneLeft extends CommandGroup {
 
-	private static final double TURN_1_ANGLE = 0;
-	private static final double TURN_1_TIMEOUT = 0;
+	private static final double SEGMENT_1 = -48; // backwards
+	private static final double SEGMENT_1_TIMEOUT = 4;
 
-	private static final double SEGMENT_1 = 0;
-	private static final double SEGMENT_1_TIMEOUT = 0;
+	private static final double TURN_1_ANGLE = -90;// left
+	private static final double TURN_1_TIMEOUT = 3;
 
-	private static final double TURN_2_ANGLE = 0;
-	private static final double TURN_2_TIMEOUT = 0;
+	private static final double SEGMENT_2 = 62.65; // forward
+	private static final double SEGMENT_2_TIMEOUT = 4;
 
-	private static final double SEGMENT_2 = 0;
-	private static final double SEGMENT_2_TIMEOUT = 0;
+	private static final double TURN_2_ANGLE = 90;// right
+	private static final double TURN_2_TIMEOUT = 3;
+
+	private static final double SEGMENT_3 = 204; // forward
+	private static final double SEGMENT_3_TIMEOUT = 4;
 
 	public SwitchLeftToNullZoneLeft() { // C to A
 
-		addSequential(new TurnWithGyro(TURN_1_ANGLE, TURN_1_TIMEOUT));
 		addSequential(new DriveForDistance(SEGMENT_1, SEGMENT_1_TIMEOUT));
-		addSequential(new TurnWithGyro(TURN_2_ANGLE, TURN_2_TIMEOUT));
+		addSequential(new TurnWithGyro(TURN_1_ANGLE, TURN_1_TIMEOUT));
 		addSequential(new DriveForDistance(SEGMENT_2, SEGMENT_2_TIMEOUT));
+		addSequential(new TurnWithGyro(TURN_2_ANGLE, TURN_2_TIMEOUT));
+		addSequential(new DriveForDistance(SEGMENT_3, SEGMENT_3_TIMEOUT));
 
 	}
 }
--- src/org/usfirst/frc/team1675/robot/commands/SwitchRightToNullZoneRight.java
+++ src/org/usfirst/frc/team1675/robot/commands/SwitchRightToNullZoneRight.java
@@ -7,23 +7,27 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class SwitchRightToNullZoneRight extends CommandGroup {
 
-	private static final double TURN_1_ANGLE = 0;
-	private static final double TURN_1_TIMEOUT = 0;
+	private static final double SEGMENT_1 = -48;
+	private static final double SEGMENT_1_TIMEOUT = 4;
 
-	private static final double SEGMENT_1 = 0;
-	private static final double SEGMENT_1_TIMEOUT = 0;
+	private static final double TURN_1_ANGLE = 90; // right turn
+	private static final double TURN_1_TIMEOUT = 3;
 
-	private static final double TURN_2_ANGLE = 0;
-	private static final double TURN_2_TIMEOUT = 0;
+	private static final double SEGMENT_2 = 60.63;
+	private static final double SEGMENT_2_TIMEOUT = 3;
 
-	private static final double SEGMENT_2 = 0;
-	private static final double SEGMENT_2_TIMEOUT = 0;
+	private static final double TURN_2_ANGLE = -90; // left turn
+	private static final double TURN_2_TIMEOUT = 3;
+
+	private static final double SEGMENT_3 = 204;
+	private static final double SEGMENT_3_TIMEOUT = 7;
 
 	public SwitchRightToNullZoneRight() { // D to B
 
-		addSequential(new TurnWithGyro(TURN_1_ANGLE, TURN_1_TIMEOUT));
 		addSequential(new DriveForDistance(SEGMENT_1, SEGMENT_1_TIMEOUT));
-		addSequential(new TurnWithGyro(TURN_2_ANGLE, TURN_2_TIMEOUT));
+		addSequential(new TurnWithGyro(TURN_1_ANGLE, TURN_1_TIMEOUT));
 		addSequential(new DriveForDistance(SEGMENT_2, SEGMENT_2_TIMEOUT));
+		addSequential(new TurnWithGyro(TURN_2_ANGLE, TURN_2_TIMEOUT));
+		addSequential(new DriveForDistance(SEGMENT_3, SEGMENT_3_TIMEOUT));
 	}
 }
