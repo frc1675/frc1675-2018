5e2887ea8ee410a8025edb83007c3aa25ae3042e
Jacob H <hakiobo7@gmail.com>
2018-02-19 15:22:46 -0600

    merged master into auto-architecture to get new Path commands (Jacob)

---
+++ src/org/usfirst/frc/team1675/robot/commands/ExchangeToPyramidLeft.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team1675.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ExchangeToPyramidLeft extends CommandGroup {
+
+	private static final double SEGMENT_1 = -147;
+	private static final double SEGMENT_1_TIMEOUT = 5;
+	
+	private static final double TURN_RIGHT_ANGLE = 135;
+	private static final double TURN_1_TIMEOUT = 5;
+	
+	private static final double SEGMENT_2 = 33.94;
+	private static final double SEGMENT_2_TIMEOUT = 5;
+	
+    public ExchangeToPyramidLeft() { // H to J
+    	
+		addSequential(new DriveForDistance(SEGMENT_1, SEGMENT_1_TIMEOUT));
+		addSequential(new TurnWithGyro(TURN_RIGHT_ANGLE, TURN_1_TIMEOUT));
+		addSequential(new DriveForDistance(SEGMENT_2, SEGMENT_2_TIMEOUT));
+		
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
--- src/org/usfirst/frc/team1675/robot/commands/MoveArmToAngle.java
+++ src/org/usfirst/frc/team1675/robot/commands/MoveArmToAngle.java
@@ -1,31 +1,28 @@
 package org.usfirst.frc.team1675.robot.commands;
 
 import org.usfirst.frc.team1675.robot.Robot;
-import org.usfirst.frc.team1675.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class MoveArm extends Command {
+public class MoveArmToAngle extends Command {
 
-    double armPower;    
-
-    public MoveArm() {
+    double angle;
+    
+    public MoveArmToAngle(double angle) {
         requires(Robot.arm);
+        this.angle = angle;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.arm.moveArm(.5, 1);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        armPower = Robot.oi.getOperatorRightYAxis();
-        Robot.arm.moveArm(armPower, .4);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -35,12 +32,10 @@ public class MoveArm extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-        Robot.arm.moveArm(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-        end();
     }
 }
