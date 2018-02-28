117c75e64b6ad034a1ca4c3764864ae5a04db473
Jacob H <hakiobo7@gmail.com>
2018-02-28 17:55:46 -0600

    Deleted unused Ramp Commands and moved solenoid activation to initialize for ramps (Jacob)

--- src/org/usfirst/frc/team1675/robot/commands/DeployRamps.java
+++ src/org/usfirst/frc/team1675/robot/commands/DeployRamps.java
@@ -15,26 +15,20 @@ public class DeployRamps extends Command {
         this.setTimeout(RobotMap.RampConstants.SOLENOID_ACTIVATION_TIME);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
         Robot.ramp.Deploy();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
+    protected void execute() {
+    }
+
     protected boolean isFinished() {
         return this.isTimedOut();
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
